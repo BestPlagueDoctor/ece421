cc21b44b8506bf0c172ff39461f12c15786d61ad
Sam Knight <sammknight@protonmail.com>
2022-09-21 11:24:00 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs;
+    utils.url = github:numtide/flake-utils;
+    poetry2nix.url = github:nix-community/poetry2nix;
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }:
+    inputs.utils.lib.eachDefaultSystem (system:
+        let
+        overlay = nixpkgs.lib.composeManyExtensions [
+        inputs.poetry2nix.overlay
+        ];
+
+        pkgs.nix = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [ overlay ];
+        };
+
+        lib = pkgs.lib;
+        stdenv = pkgs.nix.stdenv;
+        mkShell = pkgs.nix.mkShell.override { inherit stdenv; };
+
+        in {
+        devShell = mkShell {
+        packages = (with pkgs.nix; [
+            python3
+            python3Packages.poetry
+#            (poetry2nix.mkPoetryEnv { projectDir = ./.; })
+        ]);
+
+        shellHook = ''
+          export PATH=$PWD/util:$PATH
+          export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+          '';
+        };
+        }
+        );
+}
